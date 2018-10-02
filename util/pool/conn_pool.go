83711690066dd5d8caaf7bff805f07a2aef0f156
guoweilong <bjguoweilong>
2018-10-02 10:42:08 +0800

    add Copyright License

--- util/pool/conn_pool.go
+++ util/pool/conn_pool.go
@@ -2,6 +2,7 @@
 // Copyright 2018 The Silenceper Authors
 // from https://github.com/silenceper/pool.git,thanks
 //
+// The MIT License (MIT)
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
