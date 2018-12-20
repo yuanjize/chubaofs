f4988eac9cab3e6bf451543004522a669615dd05
bjguoweilong <guoweilong@jd.com>
2018-12-20 17:03:13 +0800

    go fmt code

--- datanode/partition.go
+++ datanode/partition.go
@@ -421,6 +421,7 @@ func (dp *dataPartition) updateReplicaHosts() (err error) {
 	dp.isLeader = false
 	isLeader, replicas, err := dp.fetchReplicaHosts()
 	if err != nil {
+		dp.updateReplicationTime = time.Now().Unix()
 		return
 	}
 	if !dp.compareReplicaHosts(dp.replicaHosts, replicas) {
