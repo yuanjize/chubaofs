956d9ba53c554474afbc9762ebe0cd91d7b3900a
guoweilong <bjguoweilong>
2018-12-22 23:24:22 +0800

    add support 1MB use tiny extent

--- sdk/data/stream/extent_writer.go
+++ sdk/data/stream/extent_writer.go
@@ -136,7 +136,7 @@ func (writer *ExtentWriter) write(data []byte, kernelOffset, size int) (total in
 		canWrite = writer.currentPacket.fill(data[total:size], size-total) //fill this packet
 		total += canWrite
 		if writer.IsFullCurrentPacket() || canWrite == 0 {
-			log.LogInfof("writer(%v) ", writer.toString())
+			log.LogInfof("writer(%v) total(%v) size(%v) canWrite(%v)", writer.toString(), total, size, canWrite)
 			err = writer.sendCurrPacket()
 			if err != nil { //if failed,recover it
 				return total, err
