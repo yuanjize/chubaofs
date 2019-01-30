528e346a2c6a4580dd3f7045446cf416e38cb655
bjguoweilong <guoweilong@jd.com>
2019-01-30 17:02:14 +0800

    add purgehole log

--- storage/store_extent.go
+++ storage/store_extent.go
@@ -457,7 +457,9 @@ func (s *ExtentStore) MarkDelete(extentId uint64, offset, size int64) (err error
 	}
 
 	if IsTinyExtent(extentId) {
-		return extent.DeleteTiny(offset, size)
+		err = extent.DeleteTiny(offset, size)
+		log.LogInfof("action[MarkDeleteTiny] id[%v_%v_%v_%v] err %v", s.partitionId, extentId, offset, size)
+		return err
 	}
 
 	if err = extent.MarkDelete(); err != nil {
