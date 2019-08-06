514a929b090faf831654f2ae0db9262a75fd2a51
zhuhongyin <zhuhongyin@jd.com>
2019-08-06 09:40:50 +0800

    enhancement: if the new leader and the old leader are the same node, the metadata is not reloaded
    Signed-off-by: zhuhongyin <zhuhongyin@jd.com>

--- master/master_manager.go
+++ master/master_manager.go
@@ -30,14 +30,18 @@ func (m *Master) handleLeaderChange(leader uint64) {
 		log.LogWarnf("action[handleLeaderChange] but no leader")
 		return
 	}
+	oldLeaderAddr := m.leaderInfo.addr
+	m.leaderInfo.addr = AddrDatabase[leader]
 	m.leaderInfo.addr = AddrDatabase[leader]
 	m.reverseProxy = m.newReverseProxy()
 	//Once switched to the master, the checkHeartbeat is executed
 	if m.id == leader {
 		Warn(m.clusterName, fmt.Sprintf("clusterID[%v] leader is changed to %v",
 			m.clusterName, m.leaderInfo.addr))
-		m.loadMetadata()
-		m.metaReady = true
+		if oldLeaderAddr != m.leaderInfo.addr {
+			m.loadMetadata()
+			m.metaReady = true
+		}
 		m.cluster.checkDataNodeHeartbeat()
 		m.cluster.checkMetaNodeHeartbeat()
 	} else {
