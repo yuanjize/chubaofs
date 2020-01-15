9f24e20929c2437732d3dfe31bd1f9cb03c59220
awzhgw <guowl18702995996@gmail.com>
2020-01-15 11:20:37 +0800

    Fix: if leader change,the server not open,then tryToLeader
    Signed-off-by: awzhgw <guowl18702995996@gmail.com>

--- metanode/partition_fsm.go
+++ metanode/partition_fsm.go
@@ -244,14 +244,16 @@ func (mp *metaPartition) HandleFatalEvent(err *raft.FatalError) {
 func (mp *metaPartition) HandleLeaderChange(leader uint64) {
 	ump.Alarm(UMPKey, fmt.Sprintf("LeaderChange: partition=%d, "+
 		"newLeader=%d", mp.config.PartitionId, leader))
-	conn, err := net.DialTimeout("tcp", net.JoinHostPort("127.0.0.1", serverPort), time.Second)
-	if err != nil {
-		log.LogErrorf(fmt.Sprintf("HandleLeaderChange metaPartition(%v) serverPort not exsit ,error (%v)", mp.config.PartitionId, err))
-		mp.raftPartition.TryToLeader(mp.config.PartitionId)
-		return
+	if mp.config.NodeId==leader{
+		conn, err := net.DialTimeout("tcp", net.JoinHostPort("127.0.0.1", serverPort), time.Second)
+		if err!=nil {
+			log.LogErrorf(fmt.Sprintf("HandleLeaderChange metaPartition(%v) serverPort not exsit ,error (%v)", mp.config.PartitionId, err))
+			mp.raftPartition.TryToLeader(mp.config.PartitionId)
+			return
+		}
+		conn.(*net.TCPConn).SetLinger(0)
+		conn.Close()
 	}
-	conn.(*net.TCPConn).SetLinger(0)
-	conn.Close()
 	if mp.config.NodeId != leader {
 		mp.storeChan <- &storeMsg{
 			command: stopStoreTick,
