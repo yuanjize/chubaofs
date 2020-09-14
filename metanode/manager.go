bf62fa65e959607512ebe1ebb0f3aa6c2bb5ace2
ansjsun <ansj-sun@163.com>
2020-09-14 19:02:09 +0800

    remove commit of opdelete line
    Signed-off-by: ansjsun <ansj-sun@163.com>

--- metanode/manager.go
+++ metanode/manager.go
@@ -104,7 +104,6 @@ func (m *metaManager) HandleMetaOperation(conn net.Conn, p *Packet) (err error) 
 	case proto.OpMetaLookup:
 		err = m.opMetaLookup(conn, p)
 	case proto.OpDeleteMetaPartition:
-		//err = m.opDeleteMetaPartition(conn, p)
 		err = m.opExpiredMetaPartition(conn, p)
 	case proto.OpUpdateMetaPartition:
 		err = m.opUpdateMetaPartition(conn, p)
